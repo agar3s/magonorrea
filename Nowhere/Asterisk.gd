15d69b59ddb1d4861773acd59ec121a8aaa0b721
Agar3s <ekeisco@gmail.com>
2019-10-07 13:50:55 -0500

    fix poop bug

--- Nowhere/Asterisk.gd
+++ Nowhere/Asterisk.gd
@@ -4,25 +4,31 @@ signal wizard_pooped
 
 var _poop_step = 0
 var _going_back = false
-
+var poposeado = false
 func _ready():
 	$TextureButton.connect("pressed", self, "_on_interacted")
 	self.set_frame(0)
 
 func _on_interacted():
+	if poposeado: return
+	_poop_step += 1
+	if _poop_step >= 3:
+		$TextureButton.disconnect("pressed", self, "_on_interacted")
+		
 	if _going_back:
 		_going_back = false
 		$AnimationPlayer.stop(false)
-		$AnimationPlayer.play("Poop%d" % _poop_step, 1)
+		$AnimationPlayer.play("Poop%d" % (_poop_step-1), 1)
 		yield($AnimationPlayer, "animation_finished")
 	
-	_poop_step += 1
+
 	$AnimationPlayer.play("Poop%d" % _poop_step)
 	yield($AnimationPlayer, "animation_finished")
 	
 	if _poop_step < 3:
 		_going_back = true
 		$AnimationPlayer.play("Poop%d" % _poop_step, -1, -0.2, true)
-	else:
+	elif not poposeado:
+		poposeado = true
 		$TextureButton.queue_free()
 		emit_signal("wizard_pooped")
